0f109df987a174d3823185eff82763275612a73c
daleynikov <aleynikov.dmitry@gmail.com>
2017-08-11 11:12:23 +0300

    add option.length (-shortId, +perm depts), shorting routes

--- index.js
+++ index.js
@@ -10,34 +10,35 @@ module.exports.MongooseURLShortener = Shortener;
 var express = require('express');
 
 module.exports.setRouter = function(app, connection, config) {
-	var urlShotener= new Shortener(connection);
+	
 	var router = express.Router();
 	var domain = config.domain.replace(/\/$/, "")
 	var prefix = (config.tinyUrlPrefix || "z").replace(/^\//, "").replace(/\/$/, "")
+	var urlShotener= new Shortener(connection, {prefix:prefix });
+	console.log("prefix=",prefix)
 
-	router.get("/:hash", function(req, res, next){
-		if (!req.params.hash) return res.status(400).json({status:400, success:false, message:"bookmark hash not defined" });
+	router.get("/:hash("+prefix+"*)", function(req, res, next){
+		console.log("hash", req.params.hash)
+		if (!req.params.hash) return next();
 		var result = urlShotener.resolve(req.params.hash);
 		result.then(function(data){
-			console.log(data)
 		    if (data && data.url) res.redirect(data.url)
 		    else next();
 		}).fail(function(err){
-			console.error(err)
 		    next();
 		});
 	});
 
-	router.post("/", function(req, res, next){
+	router.post("/"+prefix, function(req, res, next){
 		if (!req.body.url) return res.status(400).json({status:400, success:false, message:"bookmark URL not defined" });
 
 		var promise = urlShotener.shorten(req.body.url, req.body.data || {});
 		promise.then(function(bm){
-		    res.status(200).json({status:200, success:true, hash: bm.hash, url: domain+"/"+prefix+"/"+bm.hash});
+		    res.status(200).json({status:200, success:true, hash: bm.hash, url: domain+"/"+bm.hash});
 		}).fail(function(err){
 		    return res.status(500).json({status:500, success:false, message:"bookmark URL not generated", error:err });
 		});
 	});
 
-	app.use("/"+prefix, router);
+	app.use("/", router);
 }
